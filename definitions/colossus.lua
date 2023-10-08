fbcd61c8204ab4ea61ed7b6917f18ba95e99b4ca
Paula Lima <paulaslima93@gmail.com>
2023-10-08 17:19:29 -0300

    criou definicões para player e  criatura

---
+++ definitions/colossus.lua
@@ -0,0 +1,11 @@
+local colossus = {}
+
+colossus.name = "Prismarine"
+colossus.description = "Enormes estátuas de prismarine que medem mais de 20 metros de altura."
+colossus.maxHealth = 50
+colossus.health = 50
+colossus.attack = 5
+colossus.defense = 6
+colossus.speed = 0
+
+return colossus
--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
+--dependencies
+local player = require("definitions.player")
+local colossus = require("definitions.colossus")
+
+
+
+
 --Habilitar UTF-8 no terminal
 os.execute("chcp 65001")
 
 -- Header
-
 
 print([[
 ==================================================================
@@ -20,12 +26,11 @@ print([[
                         É hora da batalha!
       ]])
 
+--obter definição do jogador
+print(string.format("A vida do jogador é %d/%d", player.health, player.maxHealth))  -- %d recebe variável de numeros inteiros
 
 --obter definição do monstro
-
-
---obter definição do monstro
-
+local boss = colossus
 
 -- apresentar o monstro
 
