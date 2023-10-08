1ca126df9acd1d4ee05ca73bcca12d7175639d4d
Paula Lima <paulaslima93@gmail.com>
2023-10-08 16:59:30 -0300

    criando o header

---
+++ main.lua
@@ -0,0 +1,33 @@
+--Habilitar UTF-8 no terminal
+os.execute("chcp 65001")
+
+-- Header
+
+
+print([[
+==================================================================
+
+                  /| ________________
+            O|===|* >________________>
+                  \|
+        ---------------------------------------------------
+
+                    SIMULADOR DE BATALHA
+
+==================================================================
+
+        Você empunha a sua espada e se prepara para lutar.
+                        É hora da batalha!
+      ]])
+
+
+--obter definição do monstro
+
+
+--obter definição do monstro
+
+
+-- apresentar o monstro
+
+
+-- começa o loop de batalha
